225fc0524a105a703277b7990621583a656f2044
ANASWARA <anaswaraku.dev@gmail.com>
2024-11-23 22:47:20 +0530

    "Updated import paths from '/public/Core team/' to '/public/Coreteam/'"

--- src/assets/leads.js
+++ src/assets/leads.js
@@ -1,14 +1,14 @@
-import lead from '/public/Core team/Lead.jpg'
-import Operations from '/public/Core team/Operations.jpg'
-import Tech from '/public/Core team/Tech.jpg'
-import Developer from "/public/Core team/Developer.jpg";
-import ContentLead from '/public/Core team/Content.jpg'
-import ContentCo from '/public/Core team/Contentco.jpg'
-import Event from '/public/Core team/Event.jpg'
-import PR from '/public/Core team/PR.jpg'
-import PRCoLead from '/public/Core team/PRCo.jpg'
-import Finance from '/public/Core team/Finance.jpg'
-import DesignLead from '/public/Core team/Design.jpg'
+import lead from '/public/Coreteam/Lead.jpg'
+import Operations from '/public/Coreteam/Operations.jpg'
+import Tech from '/public/Coreteam/Tech.jpg'
+import Developer from "/public/Coreteam/Developer.jpg";
+import ContentLead from '/public/Coreteam/Content.jpg'
+import ContentCo from '/public/Coreteam/Contentco.jpg'
+import Event from '/public/Coreteam/Event.jpg'
+import PR from '/public/Coreteam/PR.jpg'
+import PRCoLead from '/public/Coreteam/PRCo.jpg'
+import Finance from '/public/Coreteam/Finance.jpg'
+import DesignLead from '/public/Coreteam/Design.jpg'
 
 
 export const leads = {
